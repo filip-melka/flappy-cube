fac5ab9e364038bc071be6afbedfd20dee6aaf42
filip-melka <melka.filip@proton.me>
2024-09-25 13:35:53 +0200

    feat: add player movement

--- public/js/game.js
+++ public/js/game.js
@@ -11,10 +11,18 @@ const colors = {
 const gravity = 0.12
 const jumpForce = 6
 const cornerRadius = 10
+const speed = 10
 
-const obstacles = []
+const obstacles = [
+    {
+        posX: 200,
+        spaceTop: 100,
+        spaceBottom: 200,
+    },
+]
 
 let isGameRunning = false
+let interval
 
 const player = {
     width: 100,
@@ -39,4 +47,58 @@ function drawPlayer() {
     ctx.fill()
 }
 
-drawPlayer()
+function jump() {
+    console.log("jumo")
+    if (!isGameRunning) {
+        isGameRunning = true
+        interval = setInterval(update, 20)
+    }
+    player.velocityY = -jumpForce
+}
+
+function start() {
+    clear()
+    player.posY = height / 3
+    drawPlayer()
+
+    document.body.onkeyup = function (e) {
+        if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
+            jump()
+        }
+    }
+}
+
+start()
+
+function clear() {
+    ctx.clearRect(0, 0, width, height)
+}
+
+function gameOver() {
+    console.log("game over")
+    isGameRunning = false
+    clearInterval(interval)
+    setTimeout(() => {
+        start()
+    }, 1000)
+}
+
+function update() {
+    clear()
+    updatePlayer()
+    drawPlayer()
+}
+
+function updatePlayer() {
+    player.velocityY += gravity
+    if (player.posY + player.height + player.velocityY >= height) {
+        player.posY = height - player.height
+        gameOver()
+    }
+    if (player.posY + player.height + player.velocityY <= player.height) {
+        player.posY = 0
+        gameOver()
+    } else {
+        player.posY += player.velocityY
+    }
+}
